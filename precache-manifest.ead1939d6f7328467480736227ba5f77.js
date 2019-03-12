106f4d7deffe23f52e5b9de79351402f659596d4
andraaspar <andraaspar@gmail.com>
2019-03-12 13:42:15 +0100

    Updates

---
+++ precache-manifest.ead1939d6f7328467480736227ba5f77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4520d369837306b950b9",
+    "url": "/wheel/static/js/runtime~main.b62cc40c.js"
+  },
+  {
+    "revision": "f8f2cce23a6c169971ec",
+    "url": "/wheel/static/js/main.4302d654.chunk.js"
+  },
+  {
+    "revision": "f7da6d51fa6dfb0a27fe",
+    "url": "/wheel/static/js/2.05119f4f.chunk.js"
+  },
+  {
+    "revision": "f8f2cce23a6c169971ec",
+    "url": "/wheel/static/css/main.c8c683f5.chunk.css"
+  },
+  {
+    "revision": "89d59c4c849432c960aa2204772a109e",
+    "url": "/wheel/index.html"
+  }
+];
